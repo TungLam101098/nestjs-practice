1fc00ee07d2447f50e3555a3bb89a34fc4304c5f
Lam Nguyen <lam.nguyen@asnet.com.vn>
2023-06-12 17:33:52 +0700

    refactor: remove redundant code

--- handling-io/file-watching/watch.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const file = "./file.txt";
-
-// Log current time when file change
-fs.watchFile(file, (current, previous) => {
-  return console.log(`${file} updated ${(current.mtime)}`);
-});
