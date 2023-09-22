c63bfa30b22a93b187460f9d6e9d13b56fac6687
Lam Nguyen <lam.nguyen@asnet.com.vn>
2023-09-22 11:33:58 +0700

    Feature/ Validate request for login feature

--- practice/src/routes/login.ts
+++ practice/src/routes/login.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import AuthController from '@controllers/auth-controller';
+import validate from '@middlewares/validator';
+
+import { loginValidationRules } from '@validation-rules';
 
 const router = Router();
 const authInstance = AuthController.getInstance();
 
-router.post('/', authInstance.handleLoginRequest);
+router.post('/', loginValidationRules, validate, authInstance.handleLoginRequest);
 
 export default router;
